8a4b1c5bd87ca86bd9918b84a6d7123d3528476e
Anie team <93490359+AnieTeam@users.noreply.github.com>
2022-09-03 22:39:20 +0530

    Update users_sql.py

--- AnieRobot/modules/sql/users_sql.py
+++ AnieRobot/modules/sql/users_sql.py
@@ -1,6 +1,6 @@
 import threading
 
-from sqlalchemy import Column, Integer, UnicodeText, String, ForeignKey, UniqueConstraint, func
+from sqlalchemy import Column, BigInteger, UnicodeText, String, ForeignKey, UniqueConstraint, func
 
 from AnieRobot import dispatcher
 from AnieRobot.modules.sql import BASE, SESSION
@@ -8,7 +8,7 @@ from AnieRobot.modules.sql import BASE, SESSION
 
 class Users(BASE):
     __tablename__ = "users"
-    user_id = Column(Integer, primary_key=True)
+    user_id = Column(BigInteger, primary_key=True)
     username = Column(UnicodeText)
 
     def __init__(self, user_id, username=None):
@@ -34,7 +34,7 @@ class Chats(BASE):
 
 class ChatMembers(BASE):
     __tablename__ = "chat_members"
-    priv_chat_id = Column(Integer, primary_key=True)
+    priv_chat_id = Column(BigInteger, primary_key=True)
     # NOTE: Use dual primary key instead of private primary key?
     chat = Column(String(14),
                   ForeignKey("chats.chat_id",
