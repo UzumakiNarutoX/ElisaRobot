f18443c6b0a09410ab71a0f29166e3fb60a17796
Anie team <93490359+AnieTeam@users.noreply.github.com>
2022-09-05 06:18:51 +0530

    Update language_sql.py

--- ElisaBot/modules/sql/language_sql.py
+++ ElisaBot/modules/sql/language_sql.py
@@ -1,7 +1,7 @@
 import threading
 
 from sqlalchemy import Column, String, UnicodeText
-from DarlingRobot.modules.sql import SESSION, BASE
+from ElisaBot.modules.sql import SESSION, BASE
 
 
 class ChatLangs(BASE):
